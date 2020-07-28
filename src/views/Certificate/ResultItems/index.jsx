817fa4ba0ef61a8a1f07529b89c8334cd2bdfcf0
andouillette-ys <yangshan0823@outlook.com>
2020-07-28 22:07:25 +0800

    样式优化

--- src/views/Certificate/ResultItems/index.jsx
+++ src/views/Certificate/ResultItems/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
+const firstRowNameIndex = [0, 1, 2, 3, 4]
+const secondRowNameIndex = [5, 6, 7, 8, 9]
+const thirdRowNameIndex = [10, 11, 12, 13, 14]
+const fifthRowNameIndex = [20, 21, 22, 23, 24]
+const itemNameRowIndex = [
+  ...firstRowNameIndex,
+  ...thirdRowNameIndex,
+  ...fifthRowNameIndex,
+]
+
 const ResultItems = ({ resultItems = [] }) => {
   const viewItems = buildItemView(resultItems)
   return (
     <div className="item-result__items">
-      {Object.keys(viewItems).map((key) => {
+      {Object.keys(viewItems).map((key, index) => {
+        const itemValueClass = itemNameRowIndex.includes(index)
+          ? ''
+          : 'item-result__items-value'
         return (
-          <div key={key} className="item-result__items-item">
+          <div
+            key={key}
+            className={`item-result__items-item ${itemValueClass}`}
+          >
             {viewItems[key]}
           </div>
         )
@@ -20,9 +36,7 @@ export default ResultItems
 const buildItemView = (resultItems) => {
   const result = {}
   const rowNum = 5
-  const firstRowNameIndex = [0, 1, 2, 3, 4]
-  const secondRowNameIndex = [5, 6, 7, 8, 9]
-  const thirdRowNameIndex = [10, 11, 12, 13, 14]
+
   resultItems.forEach((item, index) => {
     const prop = item.scoreMode === 1 ? 'scoreLevel' : 'score'
     if (firstRowNameIndex.includes(index)) {
