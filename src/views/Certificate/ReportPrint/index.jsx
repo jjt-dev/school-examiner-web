65384451f9022dd4049491a16bad86317ba3a2bc
yangshan <jjt-ys@outlook.com>
2020-08-04 21:46:49 +0800

    update style

--- src/views/Certificate/ReportPrint/index.jsx
+++ src/views/Certificate/ReportPrint/index.jsx
@@ -3,12 +3,21 @@ import { Tabs } from 'antd'
 import ReportVertical from '../ReportVertical'
 import ReportHoriz from '../ReportHoriz'
 import ReportVerticalWithRight from '../ReportVerticalWithRight'
+import { useSelector, useDispatch } from 'react-redux'
+import { updatePrintTabKey } from 'src/actions/app'
 
 const { TabPane } = Tabs
 
 const ReportPrint = ({ examResult }) => {
+  const dispatch = useDispatch()
+  const { printTabKey } = useSelector((state) => state.app)
+
+  const handleChange = (key) => {
+    dispatch(updatePrintTabKey(String(key)))
+  }
+
   return (
-    <Tabs defaultActiveKey="0" onChange={() => {}}>
+    <Tabs defaultActiveKey={printTabKey} onChange={handleChange}>
       <TabPane tab="竖版" key="0">
         <ReportVertical examResultContainer={examResult} />
       </TabPane>
