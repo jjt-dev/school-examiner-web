23a4c831e06bf9c6d5c3579ac54cd7ad9b6dfa4b
yangshan <jjt-ys@outlook.com>
2020-12-21 22:22:18 +0800

    投屏

--- src/views/NextGroup/index.jsx
+++ src/views/NextGroup/index.jsx
@@ -1,10 +1,9 @@
 import './index.less'
 
-import { Avatar, Button } from 'antd'
 import React, { useEffect, useState } from 'react'
 import useFetch from 'src/hooks/useFetch'
-import { addNumPrefix, getDomain } from 'src/utils/common'
 import { examRoundBroadcast } from 'src/utils/const'
+import logo from 'src/images/home_logo.png'
 
 const NextGroup = () => {
   const [examRound, setExamRound] = useState()
@@ -26,48 +25,15 @@ const NextGroup = () => {
   }, [refetch])
 
   return (
-    <div className="page next-group">
-      {nextGroup && (
-        <>
-          <Avatar
-            size={90}
-            src={`${getDomain()}${nextGroup.headerInfo.schoolLogo}`}
-            className="next-group__logo"
-          />
-          <div className="next-group__list">
-            <div className="next-group__list-round">
-              场次: {addNumPrefix(nextGroup.headerInfo.roundIndex)}
-            </div>
-            <div className="next-group__list-title">
-              <div className="next-group__list-title--level">
-                级别: {nextGroup.headerInfo.levelName}
-                {nextGroup.headerInfo.alias}
-              </div>
-              <div className="next-group__list-title--status">等待考试</div>
-            </div>
-            <div className="next-group__list-students">
-              {nextGroup.studentList.map((student) => (
-                <div
-                  key={student.studentId}
-                  className="next-group__list-students-item"
-                >
-                  <Avatar size={70} src={`${getDomain()}${student.faceUrl}`} />
-                  <span>{student.studentName}</span>
-                </div>
-              ))}
-            </div>
-            <div className="next-group__list-close">
-              <Button
-                type="primary"
-                style={{ width: '80px' }}
-                onClick={() => window.close()}
-              >
-                关闭
-              </Button>
-            </div>
-          </div>
-        </>
-      )}
+    <div className="next-group">
+      <div className="next-group-header">
+        <img src={logo} alt={logo} />
+      </div>
+      <div className="next-group-content">
+        <div className="left-content"></div>
+        <div className="middle-content"></div>
+        <div className="right-content"></div>
+      </div>
     </div>
   )
 }
