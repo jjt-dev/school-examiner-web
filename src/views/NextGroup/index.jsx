bceabc5b1166582ff70e762fc7b559990d86dcff
杨山 <shan.yang@cyber-insight.com>
2020-12-22 11:09:32 +0800

    投屏

--- src/views/NextGroup/index.jsx
+++ src/views/NextGroup/index.jsx
@@ -4,14 +4,20 @@ import React, { useEffect, useState } from 'react'
 import useFetch from 'src/hooks/useFetch'
 import { examRoundBroadcast } from 'src/utils/const'
 import logo from 'src/images/home_logo.png'
+import { Avatar } from 'antd'
+import { getDomain } from 'src/utils/common'
+const leftIndexs = [0, 1, 2, 3, 4]
+const rightIndexs = [5, 6, 7, 8, 9]
 
 const NextGroup = () => {
   const [examRound, setExamRound] = useState()
   const [nextGroup, refetch] = useFetch(`/exam/nextRound`)
+  const { studentList = [] } = examRound || {}
 
   useEffect(() => {
     examRoundBroadcast.onmessage = function (ev) {
       setExamRound(ev.data)
+      console.log(333, ev.data)
     }
     return () => examRoundBroadcast.close()
   }, [])
@@ -30,7 +36,21 @@ const NextGroup = () => {
         <img src={logo} alt={logo} />
       </div>
       <div className="next-group-content">
-        <div className="left-content"></div>
+        <div className="left-content">
+          {leftIndexs.map((index) => {
+            const student = studentList[index]
+            if (student) {
+              return (
+                <StudentResult
+                  student={student}
+                  key={index}
+                  index={index + 1}
+                />
+              )
+            }
+            return null
+          })}
+        </div>
         <div className="middle-content"></div>
         <div className="right-content"></div>
       </div>
@@ -39,3 +59,15 @@ const NextGroup = () => {
 }
 
 export default NextGroup
+
+const StudentResult = ({ index, student }) => {
+  return (
+    <div className="student-result">
+      <div class="student-result-number">{index}</div>
+      <div class="student-result-avatar">
+        <Avatar size={90} src={`${getDomain()}${student.faceUrl}`} />
+      </div>
+      <div class="student-result-item"></div>
+    </div>
+  )
+}
